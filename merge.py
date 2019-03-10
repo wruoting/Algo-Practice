57bbddfb26b29b4a8968f77431d6496e74d8cdb8
ruotingw <wruoting@gmail.com>
2019-03-10 14:47:02 -0400

    merge sort

---
+++ merge.py
@@ -0,0 +1,33 @@
+import math
+# Merge sort implementation
+# This should take 2N+2 where N is the number of elements in A
+def merge(A, p, q, r):
+    L = list()
+    R = list()
+    for i in range(p,q+1):
+        L.append(A[i])
+    for j in range(q+1,r+1):
+        R.append(A[j])
+    L.append(max(A)+1)
+    R.append(max(A)+1)
+    for k in range(p,r+1):
+        if L[0] <= R[0]:
+            A[k] = L[0]
+            L.pop(0)
+        elif L[0] > R[0]:
+            A[k] = R[0]
+            R.pop(0)
+    return A
+
+
+def merge_sort(A, p, r):
+    if p<r:
+        q = math.floor((r+p)/2)
+        merge_sort(A, p, q)
+        merge_sort(A, q+1,r)
+        merge(A, p, q, r)
+    return A
+
+A = [1, 5, 7, 12, 38, 3, 18, 21, 23, 25, 32]
+B = [3, 5, 1, 2, 6, 8]
+print(merge_sort(A, 0, len(A)-1))
