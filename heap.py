d2adb9ef6cb2d0486eb19e93262b27530d3d47ff
robert <robert@numerated.com>
2019-03-11 13:08:50 -0400

    logic works now

--- heap.py
+++ heap.py
@@ -7,17 +7,18 @@ def max_heapify(A, i):
     if left_index < len(A):
         if A[i] < A[left_index]:
             largest = left_index
-            temp = A[i]
-            A[i] = A[left_index]
-            A[left_index] = temp
     if right_index < len(A):
         if A[i] < A[right_index]:
-            largest = right_index
-            temp = A[i]
-            A[i] = A[right_index]
-            A[right_index] = temp
+	    if largest != i:
+	    	if A[right_index] > A[left_index]:
+			largest = right_index
+            elif largest == i:
+		largest = right_index
     if largest == i:
         return A
+    temp = A[i]
+    A[i] = A[largest]
+    A[largest] = temp
     return max_heapify(A, largest)
 
 def build_heap(A):
@@ -26,7 +27,7 @@ def build_heap(A):
     return A
 
 A = [9, 3, 16, 1, 2, 8, 9]
-A = [6, 2, 5, 8, 10, 12, 17, 2, 1]
+A = [6, 8, 12, 7,  5, 8, 10, 12, 17, 2, 1]
 print(build_heap(A))
 A.pop(0)
 print(build_heap(A))
