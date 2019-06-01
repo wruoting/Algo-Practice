6b1c9a91192dd9c89301c9817dc0b29d3b455d5b
ruotingw <wruoting@gmail.com>
2019-06-01 09:12:35 -0400

    update heap

--- heap.py
+++ heap.py
@@ -1,33 +1,43 @@
 import math
 def max_heapify(A, i):
-    left_index = 2*(i+1)-1
-    right_index = 2*(i+1)
-
+    left = (i+1)*2 - 1
+    right = (i+1)*2
     largest = i
-    if left_index < len(A):
-        if A[i] < A[left_index]:
-            largest = left_index
-    if right_index < len(A):
-        if A[i] < A[right_index]:
-	    if largest != i:
-	    	if A[right_index] > A[left_index]:
-			largest = right_index
-            elif largest == i:
-		largest = right_index
-    if largest == i:
-        return A
-    temp = A[i]
-    A[i] = A[largest]
-    A[largest] = temp
-    return max_heapify(A, largest)
-
+    if left < len(A) and A[left] > A[i]:
+        largest = left
+    if right < len(A) and A[right] > A[i]:
+        if largest == left and A[right]> A[left]:
+            largest = right
+        elif largest == i:
+            largest = right
+    if largest != i:
+        temp = A[largest]
+        A[largest] = A[i]
+        A[i] = temp
+        return max_heapify(A, largest)
+    return A
 def build_heap(A):
-    for i in range(int(math.floor(len(A)/2)),-1,-1):
+    index = int(math.floor(len(A)/2))-1
+    for i in range(index, -1, -1):
         max_heapify(A, i)
     return A
 
+def swim_up(i, A):
+    if i>0 and A[i] > A[int(math.floor((i-1)/2))]:
+        # swap i and i/2
+        temp = A[i]
+        A[i] = A[int(math.floor((i-1)/2))]
+        A[int(math.floor((i-1)/2))] = temp
+        return swim_up(int(math.floor((i-1)/2)), A)
+    return A
+    
 A = [9, 3, 16, 1, 2, 8, 9]
-A = [6, 8, 12, 7,  5, 8, 10, 12, 17, 2, 1]
+A = [6, 8, 12, 7, 5, 8, 10, 12, 17, 2, 1]
+# print(build_heap(A))
+# A.pop(0)
 print(build_heap(A))
-A.pop(0)
-print(build_heap(A))
+
+A.append(20)
+print(A)
+swim_up(len(A)-1, A)
+print(A)
